88d63d8263c16c6cdffee18841d63ebd5c3a9f28
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-07-24 14:35:23 -0700

    Use one with asterick

--- infrastructure/vault_cluster/main.tf
+++ infrastructure/vault_cluster/main.tf
@@ -13,7 +13,7 @@ resource "hcp_vault_cluster" "vault_cluster" {
   count = var.enabled ? 1 : 0
 
   cluster_id      = "jasons-vault-cluster"
-  hvn_id          = hcp_hvn.hvn_aws[0].hvn_id
+  hvn_id          = one(hcp_hvn.hvn_aws[*].hvn_id)
   public_endpoint = true
   tier            = "dev"
 }
@@ -21,12 +21,12 @@ resource "hcp_vault_cluster" "vault_cluster" {
 ## Output
 
 output "vault_cluster_id" {
-  value     = hcp_vault_cluster.vault_cluster.*.cluster_id
+  value     = one(hcp_vault_cluster.vault_cluster[*].cluster_id)
   sensitive = true
 }
 
 output "vault_cluster_public_address" {
-  value     = hcp_vault_cluster.vault_cluster.*.vault_public_endpoint_url
+  value     = one(hcp_vault_cluster.vault_cluster[*].vault_public_endpoint_url)
   sensitive = true
 }
 
@@ -35,12 +35,12 @@ output "vault_cluster_public_address" {
 resource "hcp_vault_cluster_admin_token" "cluster_token" {
   count = var.enabled ? 1 : 0
 
-  cluster_id = hcp_vault_cluster.vault_cluster[0].cluster_id
+  cluster_id = one(hcp_vault_cluster.vault_cluster[*].cluster_id)
 }
 
 ## Output
 
 output "vault_cluster_token" {
-  value     = hcp_vault_cluster_admin_token.cluster_token.*.token
+  value     = one(hcp_vault_cluster_admin_token.cluster_token[*].token)
   sensitive = true
 }
