f1371e4e10ac0e237b522a2988dcf2b2a40b7e19
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-07-24 13:57:25 -0700

    Comment out creating cluster token

--- infrastructure/vault_manifests/provider.tf
+++ infrastructure/vault_manifests/provider.tf
@@ -6,15 +6,15 @@ data "hcp_vault_cluster" "vault" {
   # ]
 }
 
-resource "hcp_vault_cluster_admin_token" "cluster_token" {
-  count = var.enabled ? 1 : 0
+# resource "hcp_vault_cluster_admin_token" "cluster_token" {
+#   count = var.enabled ? 1 : 0
 
-  cluster_id = var.cluster_id
+#   cluster_id = var.cluster_id
 
-  # depends_on = [
-  #   digitalocean_kubernetes_cluster.k8s_cluster
-  # ]
-}
+#   # depends_on = [
+#   #   digitalocean_kubernetes_cluster.k8s_cluster
+#   # ]
+# }
 
 # Set `VAULT_ADDR` and `VAULT_TOKEN`
 # REF: https://registry.terraform.io/providers/hashicorp/vault/latest/docs
