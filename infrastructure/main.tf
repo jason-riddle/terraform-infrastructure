b3adceb29f3497dac6de3610eccd5a429a62164a
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-01 14:47:02 -0700

    Remove backup user

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -64,18 +64,18 @@ output "system_secret_access_key_ssm_path" {
 
 module "aws_terraform_infrastructure_backup_system_user" {
   source  = "./aws_users/aws_terraform_infrastructure_backup_system_user"
-  enabled = true
+  enabled = false
 }
 
-output "backup_system_access_key_id_ssm_path" {
-  value     = module.aws_terraform_infrastructure_backup_system_user.access_key_id_ssm_path
-  sensitive = true
-}
+# output "backup_system_access_key_id_ssm_path" {
+#   value     = module.aws_terraform_infrastructure_backup_system_user.access_key_id_ssm_path
+#   sensitive = true
+# }
 
-output "backup_system_secret_access_key_ssm_path" {
-  value     = module.aws_terraform_infrastructure_backup_system_user.secret_access_key_ssm_path
-  sensitive = true
-}
+# output "backup_system_secret_access_key_ssm_path" {
+#   value     = module.aws_terraform_infrastructure_backup_system_user.secret_access_key_ssm_path
+#   sensitive = true
+# }
 
 ## Cloudflare
 
