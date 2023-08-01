a40bb732f65f652a0bfe0f6744e764db4397ca97
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-01 15:55:57 -0700

    Add comments

--- infrastructure/google_gcp_service_accounts/main.tf
+++ infrastructure/google_gcp_service_accounts/main.tf
@@ -1,3 +1,7 @@
+# IAM Policy Troubleshooter: https://console.cloud.google.com/iam-admin/troubleshooter
+
+# Requires: iam.googleapis.com
+# https://console.developers.google.com/apis/api/iam.googleapis.com/overview?project=425848421470
 resource "google_service_account" "service_account" {
   count = var.enabled ? 1 : 0
 
