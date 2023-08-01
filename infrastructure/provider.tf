d151c61afcf238891ba8332fb466c161c3f91ecb
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-01 15:29:12 -0700

    Add more comments for service account

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -39,9 +39,13 @@ provider "github" {}
 # Set `GOOGLE_CREDENTIALS`
 # DOCS: https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 # Credentials
-# > Name:
-# > Permissions:
-# > Settings URL:
+# > Name: tf-test-account
+# > Permissions: Service Account User
+# > Settings URL: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/permissions?project=starlit-factor-394405
+# > Other Notes:
+# >> Service Account Role: ?
+# >> Service Account Permissions: ?
+# >> Service Account Keys: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/keys?project=starlit-factor-394405
 provider "google" {}
 
 ## HCP
