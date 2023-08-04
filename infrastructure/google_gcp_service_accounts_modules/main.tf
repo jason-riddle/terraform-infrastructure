4234efc89707ee8b3b7d95c47b79fc49e354a119
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-03 18:14:20 -0700

    Run format

--- infrastructure/google_gcp_service_accounts_modules/main.tf
+++ infrastructure/google_gcp_service_accounts_modules/main.tf
@@ -1,4 +1,4 @@
 module "service_accounts" {
-  source        = "terraform-google-modules/service-accounts/google"
-  version       = "~> 3.0"
+  source  = "terraform-google-modules/service-accounts/google"
+  version = "~> 3.0"
 }
