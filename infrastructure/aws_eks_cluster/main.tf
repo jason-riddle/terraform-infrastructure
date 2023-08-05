2cbc55694dbdba34e412b7a30851a54f28d8f738
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-04 21:44:38 -0700

    Add other resources

--- infrastructure/aws_eks_cluster/main.tf
+++ infrastructure/aws_eks_cluster/main.tf
@@ -9,6 +9,35 @@ module "label" {
   environment = "infrastructure"
 }
 
+## EKS - PREQS
+
+module "aws_eks_vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "2.1.0"
+  enabled = var.enabled
+
+  cidr_block = "172.16.0.0/16"
+
+  ipv6_egress_only_internet_gateway_enabled = false
+
+  context = module.label.context
+}
+
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.4.1"
+  enabled = var.enabled
+
+  vpc_id     = module.aws_eks_vpc.vpc_id
+  igw_id     = module.aws_eks_vpc.igw_id
+  cidr_block = module.aws_eks_vpc.vpc_cidr_block
+
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
+
+  context = module.label.context
+}
+
 ## EKS
 
 module "aws_eks_cluster" {
@@ -16,6 +45,6 @@ module "aws_eks_cluster" {
   version = "2.9.0"
   enabled = var.enabled
 
-  # source  = "cloudposse/eks-cluster/aws//examples/complete"
-
+  vpc_id     = module.aws_eks_vpc.vpc_id
+  subnet_ids = module.aws_eks_subnets.public_subnet_ids
 }
