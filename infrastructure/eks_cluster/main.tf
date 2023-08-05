a1cdaedf747013e48d3af7b21b25402b08b9ed0a
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-04 22:46:29 -0700

    Update

--- infrastructure/eks_cluster/main.tf
+++ infrastructure/eks_cluster/main.tf
@@ -37,5 +37,7 @@ module "eks_cluster" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.subnets.public_subnet_ids
 
+  cluster_encryption_config_enabled = false
+
   context = module.this.context
 }
