f9e374b645aedce92f263dd9355894a8035533bd
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-04 22:26:37 -0700

    Update

--- infrastructure/eks_cluster/main.tf
+++ infrastructure/eks_cluster/main.tf
@@ -20,7 +20,7 @@ module "subnets" {
 
   vpc_id     = module.vpc.vpc_id
   igw_id     = module.vpc.igw_id
-  cidr_block = module.vpc.vpc_cidr_block
+  # cidr_block = module.vpc.vpc_cidr_block
 
   nat_gateway_enabled  = false
   nat_instance_enabled = false
