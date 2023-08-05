02deaa88e21cd08a776b700e18157e7df9e1ef74
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-04 21:07:41 -0700

    More cleanup

---
+++ infrastructure/new_aws_cli_user/main.tf
@@ -0,0 +1,37 @@
+## Label
+
+module "label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  enabled = true
+
+  namespace   = "terraform"
+  environment = "infrastructure"
+}
+
+module "aws_cli_user" {
+  source  = "cloudposse/iam-system-user/aws"
+  version = "1.1.0"
+  enabled = var.enabled
+
+  ssm_enabled                   = true
+  ssm_ses_smtp_password_enabled = false
+
+  name = "jason"
+
+  policy_arns_map = {
+    admin = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+
+  context = module.label.context
+}
+
+## Output
+
+output "access_key_id_ssm_path" {
+  value = module.aws_cli_user.access_key_id_ssm_path
+}
+
+output "secret_access_key_ssm_path" {
+  value = module.aws_cli_user.secret_access_key_ssm_path
+}
