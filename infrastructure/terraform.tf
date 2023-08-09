311160f0caaa93cf0330d70d6319e8ad0ec7c526
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 15:24:52 -0700

    Add cloud info block

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -2,7 +2,11 @@ terraform {
   required_version = ">= 1.0.0"
 
   cloud {
-    #
+    organization = "org-jasonriddle"
+
+    workspaces {
+      name = "terraform-infrastructure"
+    }
   }
 
   required_providers {
