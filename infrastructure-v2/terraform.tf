73a2085a41893746d4c1285e72bc04268276c27c
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 15:14:51 -0700

    Add tailscale provider docs

--- infrastructure-v2/terraform.tf
+++ infrastructure-v2/terraform.tf
@@ -6,6 +6,25 @@ terraform {
   }
 
   required_providers {
-    #
+    # Docs:
+    # - https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
+    tailscale = {
+      source = "tailscale/tailscale"
+    }
   }
 }
+
+# Tailscale
+
+# Set:
+# - `TAILSCALE_API_KEY`
+# - `TAILSCALE_TAILNET`
+#
+# Docs:
+# - https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
+#
+# Credentials:
+# - Name:
+# - Permissions:
+# - Settings URL: https://login.tailscale.com/admin/settings/keys
+provider "tailscale" {}
