37fbeeee64f7028ca7deb1d4396debbccb943aab
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 15:24:09 -0700

    Add cloudflare

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -7,6 +7,12 @@ terraform {
 
   required_providers {
     # Docs:
+    # - https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+
+    # Docs:
     # - https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
     tailscale = {
       source = "tailscale/tailscale"
@@ -14,14 +20,29 @@ terraform {
   }
 }
 
+# Cloudflare
+#
+# Docs:
+# - https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
+#
+# Export:
+# - `CLOUDFLARE_API_KEY` and `CLOUDFLARE_EMAIL`
+# OR
+# - `CLOUDFLARE_API_TOKEN`
+#
+# Credentials
+# - Name:
+# - Permissions:
+# - Settings URL: https://dash.cloudflare.com/profile/api-tokens
+provider "cloudflare" {}
+
 # Tailscale
 #
 # Docs:
 # - https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
 #
 # Export:
-# - `TAILSCALE_API_KEY`
-# - `TAILSCALE_TAILNET`
+# - `TAILSCALE_API_KEY` and `TAILSCALE_TAILNET`
 #
 # Credentials:
 # - Name:
