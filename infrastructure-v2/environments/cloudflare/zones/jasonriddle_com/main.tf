9fd0ea5c17057155ea41b52b36b8d92e50b43799
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 15:01:12 -0700

    Add cloudflare zone and rest of terraform.tf

---
+++ infrastructure-v2/environments/cloudflare/zones/jasonriddle_com/main.tf
@@ -0,0 +1,3 @@
+data "cloudflare_zone" "jasonriddle_com" {
+  name = "jasonriddle.com"
+}
--- infrastructure-v2/terraform.tf
+++ infrastructure-v2/terraform.tf
@@ -1,3 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  cloud {
+    #
+  }
+
+  required_providers {
+    #
+  }
 }
