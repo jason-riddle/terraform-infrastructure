e53fcb439b9e297b276290c996ed94ef8a8625a7
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 21:47:13 -0700

    Uncomment provider

--- infrastructure/cloudflare/zones/riddleapps_net/terraform.tf
+++ infrastructure/cloudflare/zones/riddleapps_net/terraform.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   required_version = ">= 1.0.0"
+terraform {
+  required_version = ">= 1.0.0"
 
-#   required_providers {
-#     cloudflare = {
-#       source = "cloudflare/cloudflare"
-#     }
-#   }
-# }
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
+}
--- infrastructure/github/repos/terraform_infrastructure/terraform.tf
+++ infrastructure/github/repos/terraform_infrastructure/terraform.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   required_version = ">= 1.0.0"
+terraform {
+  required_version = ">= 1.0.0"
 
-#   required_providers {
-#     github = {
-#       source = "integrations/github"
-#     }
-#   }
-# }
+  required_providers {
+    github = {
+      source = "integrations/github"
+    }
+  }
+}
