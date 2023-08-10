0e3e06d674982d5d6f09f5ee8bff82db25c75510
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-10 13:40:36 -0700

    Add tailscale environment

---
+++ infrastructure/github/repos/ansible_role_tailscale/main.tf
@@ -0,0 +1,38 @@
+data "github_repository" "repo" {
+  full_name = "jason-riddle/ansible-role-tailscale"
+}
+
+# resource "github_branch_protection" "main" {
+#   count = 1
+
+#   repository_id = data.github_repository.repo.node_id
+
+#   pattern = "main"
+
+#   allows_deletions        = false
+#   allows_force_pushes     = false
+#   enforce_admins          = false
+#   required_linear_history = true
+
+#   required_status_checks {
+#     strict = true
+#   }
+# }
+
+# resource "github_repository_environment" "environment" {
+#   repository  = data.github_repository.repo.name
+#   environment = "environment"
+# }
+
+# resource "github_actions_environment_secret" "secret" {
+#   repository      = data.github_repository.repo.name
+#   environment     = github_repository_environment.environment.environment
+#   secret_name     = "secret_name"
+#   plaintext_value = "%s"
+# }
+
+resource "github_actions_secret" "secret" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "example_secret_name"
+  plaintext_value = "123"
+}
