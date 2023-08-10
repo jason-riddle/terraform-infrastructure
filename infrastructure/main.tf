212bff9713e08807f15d2bae19cc5a23e19c2b28
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-09 21:10:21 -0700

    Add tailscale

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,3 +2,8 @@
 module "cf_jasonriddle_com" {
   source = "./cloudflare/zones/jasonriddle_com"
 }
+
+# Tailscale
+module "ts_jasonriddle11_gmail_com" {
+  source = "./tailscale/tailnets/jasonriddle11_gmail_com"
+}
