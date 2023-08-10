7829f28b337b9025e589644e1f1800fc814c9943
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-10 13:36:08 -0700

    Remove branch protections and env actions secrets

--- infrastructure/github/repos/terraform_infrastructure/main.tf
+++ infrastructure/github/repos/terraform_infrastructure/main.tf
@@ -2,37 +2,37 @@ data "github_repository" "repo" {
   full_name = "jason-riddle/terraform-infrastructure"
 }
 
-resource "github_branch_protection" "main" {
-  count = 1
+# resource "github_branch_protection" "main" {
+#   count = 1
 
-  repository_id = data.github_repository.repo.node_id
+#   repository_id = data.github_repository.repo.node_id
 
-  pattern = "main"
+#   pattern = "main"
 
-  allows_deletions        = false
-  allows_force_pushes     = false
-  enforce_admins          = false
-  required_linear_history = true
+#   allows_deletions        = false
+#   allows_force_pushes     = false
+#   enforce_admins          = false
+#   required_linear_history = true
 
-  required_status_checks {
-    strict = true
-  }
-}
+#   required_status_checks {
+#     strict = true
+#   }
+# }
 
 resource "github_repository_environment" "environment" {
   repository  = data.github_repository.repo.name
   environment = "environment"
 }
 
-resource "github_actions_environment_secret" "secret" {
-  repository      = data.github_repository.repo.name
-  environment     = github_repository_environment.environment.environment
-  secret_name     = "secret_name"
-  plaintext_value = "%s"
-}
+# resource "github_actions_environment_secret" "secret" {
+#   repository      = data.github_repository.repo.name
+#   environment     = github_repository_environment.environment.environment
+#   secret_name     = "secret_name"
+#   plaintext_value = "%s"
+# }
 
-resource "github_actions_secret" "secret" {
-  repository      = "example_repository"
-  secret_name     = "example_secret_name"
-  plaintext_value = "123"
-}
+# resource "github_actions_secret" "secret" {
+#   repository      = "example_repository"
+#   secret_name     = "example_secret_name"
+#   plaintext_value = "123"
+# }
