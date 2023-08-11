a989d9ebb152c9ca75468559753668473619477c
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-10 19:47:08 -0700

    Remove default

--- infrastructure/github/repos/ansible_role_tailscale/variables.tf
+++ infrastructure/github/repos/ansible_role_tailscale/variables.tf
@@ -1,10 +1,8 @@
 variable "secret_name" {
-  type    = string
-  default = "EXAMPLE_SECRET_NAME"
+  type = string
 }
 
 variable "secret_plaintext_value" {
   type      = string
-  default   = "123"
   sensitive = true
 }
