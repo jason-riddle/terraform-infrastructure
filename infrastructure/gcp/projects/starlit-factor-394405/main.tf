9f0f6a143f3ca65d1353f3832156a2a8043ab47f
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-14 20:47:36 -0700

    Add service account key

--- infrastructure/gcp/projects/starlit-factor-394405/main.tf
+++ infrastructure/gcp/projects/starlit-factor-394405/main.tf
@@ -2,3 +2,15 @@ resource "google_service_account" "service_account" {
   account_id   = "service-account-example-id"
   display_name = "Service Account Example Name"
 }
+
+resource "time_rotating" "rotation" {
+  rotation_days = 30
+}
+
+resource "google_service_account_key" "key" {
+  service_account_id = google_service_account.service_account.name
+
+  keepers = {
+    rotation_time = time_rotating.rotation.rotation_rfc3339
+  }
+}
