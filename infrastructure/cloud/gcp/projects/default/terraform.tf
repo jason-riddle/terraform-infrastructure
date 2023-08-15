7b2e0b5ab34e8cea27ac5c3c7872e2cdd1d9c7e1
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-14 20:15:39 -0700

    Remove service account create example

--- infrastructure/cloud/gcp/projects/default/terraform.tf
+++ infrastructure/cloud/gcp/projects/default/terraform.tf
@@ -26,11 +26,7 @@ terraform {
 # >> Service Account Permissions: ?
 # >> Service Account Keys: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/keys?project=starlit-factor-394405
 #
-# Create a new service account: https://cloud.google.com/iam/docs/service-accounts-create#iam-service-accounts-create-gcloud
-#
-# gcloud iam service-accounts create SA_NAME \
-#   --description="DESCRIPTION \
-#   --display-name="DISPLAY_NAME
+# Create a new service account: https://cloud.google.com/iam/docs/service-accounts-create
 provider "google" {
   project = "starlit-factor-394405"
 }
