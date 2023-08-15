187d1e77b1d668b0d003ce82bab2baf85fce9d04
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-14 20:39:37 -0700

    Move github provider out

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -8,26 +8,4 @@ terraform {
       name = "terraform-infrastructure"
     }
   }
-
-  required_providers {
-    # Docs:
-    # - https://registry.terraform.io/providers/integrations/github/latest/docs
-    github = {
-      source = "integrations/github"
-    }
-  }
 }
-
-# Github
-#
-# Docs:
-# - https://registry.terraform.io/providers/integrations/github/latest/docs
-#
-# Export:
-# - `GITHUB_TOKEN`
-#
-# Credentials
-# - Name: terraform-infrastructure-system-user
-# - Permissions: public_repo, read:org
-# - Settings URL: https://github.com/settings/tokens/1270530699
-provider "github" {}
