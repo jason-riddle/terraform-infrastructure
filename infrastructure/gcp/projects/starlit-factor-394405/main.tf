4ee90bc1e0c826b54c856a8aeb09054d6564fdb4
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-14 20:41:12 -0700

    Add example to service account name

--- infrastructure/gcp/projects/starlit-factor-394405/main.tf
+++ infrastructure/gcp/projects/starlit-factor-394405/main.tf
@@ -1,4 +1,4 @@
 resource "google_service_account" "service_account" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
+  account_id   = "service-account-example-id"
+  display_name = "Service Account Example Name"
 }
