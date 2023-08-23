e38fc2ff6b5c89d3e721973911d9f448909e1408
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-22 19:34:16 -0700

    Update comments

--- infrastructure/gcp/projects/starlit-factor-394405/terraform.tf
+++ infrastructure/gcp/projects/starlit-factor-394405/terraform.tf
@@ -14,17 +14,13 @@ terraform {
 # - https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 #
 # Export:
-# - `GOOGLE_CREDENTIALS`, `GOOGLE_PROJECT`, `GOOGLE_REGION`, `GOOGLE_ZONE`
+# - `GOOGLE_CREDENTIALS`
 #
-# Credentials
+# Service Account Credentials
 # - Name: tf-test-account
-# - Permissions: Service Account User
-# - Settings URL: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/permissions?project=starlit-factor-394405
-#
-# > Other Notes:
-# >> Service Account Role: ?
-# >> Service Account Permissions: ?
-# >> Service Account Keys: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/keys?project=starlit-factor-394405
+# - Permissions: Owner, Service Account User
+# - Permissions URL: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/permissions?project=starlit-factor-394405
+# - Keys: https://console.cloud.google.com/iam-admin/serviceaccounts/details/112222367733525882864/keys?project=starlit-factor-394405
 #
 # Create a new service account: https://cloud.google.com/iam/docs/service-accounts-create
 provider "google" {
