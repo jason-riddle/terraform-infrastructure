3e281bfcd01de29c7f7887b0f5a83fa06155e6ff
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-08-28 16:04:58 -0600

    Remove unneeded keys

--- infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -1,48 +1,3 @@
-resource "tailscale_tailnet_key" "pi_authkey" {
-  count = 1
-
-  reusable      = true
-  ephemeral     = false
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:pi"]
-}
-
-output "pi_authkey" {
-  value     = tailscale_tailnet_key.pi_authkey[0].key
-  sensitive = true
-}
-
-resource "tailscale_tailnet_key" "pi_cluster_authkey" {
-  count = 1
-
-  reusable      = true
-  ephemeral     = false
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:pi-cluster"]
-}
-
-output "pi_cluster_authkey" {
-  value     = tailscale_tailnet_key.pi_cluster_authkey[0].key
-  sensitive = true
-}
-
-resource "tailscale_tailnet_key" "github_actions_authkey" {
-  count = 1
-
-  reusable      = true
-  ephemeral     = true
-  preauthorized = true
-  expiry        = 7776000 # 90 Days
-  tags          = ["tag:github-actions"]
-}
-
-output "github_actions_authkey" {
-  value     = tailscale_tailnet_key.github_actions_authkey[0].key
-  sensitive = true
-}
-
 resource "tailscale_tailnet_key" "github_actions_ansible_role_tailscale_authkey" {
   count = 1
 
@@ -58,17 +13,17 @@ output "github_actions_ansible_role_tailscale_authkey" {
   sensitive = true
 }
 
-resource "tailscale_tailnet_key" "k3s_subnet_router_authkey" {
+resource "tailscale_tailnet_key" "pi_cluster_authkey" {
   count = 1
 
   reusable      = true
   ephemeral     = false
   preauthorized = true
   expiry        = 7776000 # 90 Days
-  tags          = ["tag:k3s"]
+  tags          = ["tag:pi-cluster"]
 }
 
-output "k3s_subnet_router_authkey" {
-  value     = tailscale_tailnet_key.k3s_subnet_router_authkey[0].key
+output "pi_cluster_authkey" {
+  value     = tailscale_tailnet_key.pi_cluster_authkey[0].key
   sensitive = true
 }
