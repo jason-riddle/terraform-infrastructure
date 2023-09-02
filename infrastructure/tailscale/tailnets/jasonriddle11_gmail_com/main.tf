f1a520f9df71dc415bbd3211a41e3250e9e58ecc
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-09-01 22:46:55 -0600

    Add subnet router auth key

--- infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
+++ infrastructure/tailscale/tailnets/jasonriddle11_gmail_com/main.tf
@@ -27,3 +27,18 @@ output "pi_cluster_authkey" {
   value     = tailscale_tailnet_key.pi_cluster_authkey[0].key
   sensitive = true
 }
+
+resource "tailscale_tailnet_key" "subnet_router_authkey" {
+  count = 1
+
+  reusable      = true
+  ephemeral     = false
+  preauthorized = true
+  expiry        = 7776000 # 90 Days
+  tags          = ["tag:subnet-router"]
+}
+
+output "subnet_router_authkey" {
+  value     = tailscale_tailnet_key.subnet_router_authkey[0].key
+  sensitive = true
+}
