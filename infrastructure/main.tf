0cb8d944ea04f3989dadf3c6be379c37e0eaf617
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-09-01 22:47:57 -0600

    Add subnet router authkey

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,6 +37,11 @@ output "ts_pi_cluster_authkey" {
   sensitive = true
 }
 
+output "ts_subnet_router_authkey" {
+  value     = module.ts_jasonriddle11_gmail_com.subnet_router_authkey
+  sensitive = true
+}
+
 # Azure
 # module "azure_subscriptions_default" {
 #   source = "./azure/subscriptions/default"
