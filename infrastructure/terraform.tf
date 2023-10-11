0e5957a5081fe79325896294f8bfa702bb639bb5
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-10-10 23:28:12 -0700

    Centralize terraform.tf

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -8,4 +8,30 @@ terraform {
       name = "terraform-infrastructure"
     }
   }
+
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    # https://us-east-1.console.aws.amazon.com/iamv2/home?region=us-east-1#/users/details/Jason-AWSCLI?section=permissions
+    # AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    # https://registry.terraform.io/providers/integrations/github/latest/docs
+    # https://registry.terraform.io/providers/integrations/github/latest/docs
+    # GITHUB_TOKEN
+    # https://github.com/settings/tokens/1270530699
+    # Name: terraform-infrastructure-system-user
+    # Permissions: public_repo, read:org
+    github = {
+      source = "integrations/github"
+    }
+
+    # https://registry.terraform.io/providers/tailscale/tailscale/latest/docs
+    # https://login.tailscale.com/admin/settings/keys
+    # TAILSCALE_API_KEY, TAILSCALE_TAILNET
+    tailscale = {
+      source = "tailscale/tailscale"
+    }
+  }
 }
