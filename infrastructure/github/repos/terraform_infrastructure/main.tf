f27e0819531f73d691b4cb2278da2eecd6628c9c
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-10-10 23:23:36 -0700

    Comment out branch protections

--- infrastructure/github/repos/terraform_infrastructure/main.tf
+++ infrastructure/github/repos/terraform_infrastructure/main.tf
@@ -2,19 +2,19 @@ data "github_repository" "repo" {
   full_name = "jason-riddle/terraform-infrastructure"
 }
 
-resource "github_branch_protection" "main" {
-  count = 1
+# resource "github_branch_protection" "main" {
+#   count = 1
 
-  repository_id = data.github_repository.repo.node_id
+#   repository_id = data.github_repository.repo.node_id
 
-  pattern = "main"
+#   pattern = "main"
 
-  allows_deletions        = false
-  allows_force_pushes     = false
-  enforce_admins          = false
-  required_linear_history = true
+#   allows_deletions        = false
+#   allows_force_pushes     = false
+#   enforce_admins          = false
+#   required_linear_history = true
 
-  required_status_checks {
-    strict = true
-  }
-}
+#   required_status_checks {
+#     strict = true
+#   }
+# }
