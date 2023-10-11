04c2a5b7733e23508274a9fbb762b9ed52823504
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-10-10 23:19:14 -0700

    Don't init aws module twice

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,8 +40,3 @@ output "ts_pi_cluster_authkey" {
   value     = module.ts_jasonriddle11_gmail_com.pi_cluster_authkey
   sensitive = true
 }
-
-# AWS
-module "aws_accounts_personal" {
-  source = "./aws/accounts/personal"
-}
