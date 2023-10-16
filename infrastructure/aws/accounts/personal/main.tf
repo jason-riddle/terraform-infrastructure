83d151dddba01f912c51362496c37e03cf1bb385
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-10-15 17:22:04 -0700

    Add a user

--- infrastructure/aws/accounts/personal/main.tf
+++ infrastructure/aws/accounts/personal/main.tf
@@ -1,6 +1,20 @@
+module "iam_user1" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.30.0"
+
+  name = "user1"
+
+  create_iam_user_login_profile = false
+  create_iam_access_key         = false
+}
+
 module "group" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
   version = "5.30.0"
 
   name = "testgroup"
+
+  group_users = [
+    module.iam_user1.iam_user_name,
+  ]
 }
