20043f0a5121af5eaea213dc9c11ba83cbff93d2
Jason Riddle <2156866+jason-riddle@users.noreply.github.com>
2023-10-15 18:01:51 -0700

    Add oidc provider

--- infrastructure/aws/accounts/personal/main.tf
+++ infrastructure/aws/accounts/personal/main.tf
@@ -20,3 +20,29 @@ module "group" {
     module.iam_user1.iam_user_name,
   ]
 }
+
+module "iam_github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "5.30.0"
+}
+
+# module "iam_github_oidc_role" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+#   version = "5.30.0"
+
+#   name = local.name
+
+#   # This should be updated to suit your organization, repository, references/branches, etc.
+#   subjects = [
+#     # You can prepend with `repo:` but it is not required
+#     "repo:terraform-aws-modules/terraform-aws-iam:pull_request",
+#     "terraform-aws-modules/terraform-aws-iam:ref:refs/heads/master",
+#   ]
+
+#   policies = {
+#     additional = aws_iam_policy.additional.arn
+#     S3ReadOnly = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+#   }
+
+#   tags = local.tags
+# }
